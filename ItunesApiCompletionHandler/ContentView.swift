4cc8a20d7a4ef203e6297f42e2ef0aac25202307
peter <apppeterpan@gmail.com>
2022-12-01 16:06:08 +0800

    Initial Commit

---
+++ ItunesApiCompletionHandler/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ItunesApiCompletionHandler
+//
+//  Created by Peter Pan on 2022/12/1.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ItunesApiCompletionHandler/ItunesApiCompletionHandlerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ItunesApiCompletionHandlerApp.swift
+//  ItunesApiCompletionHandler
+//
+//  Created by Peter Pan on 2022/12/1.
+//
+
+import SwiftUI
+
+@main
+struct ItunesApiCompletionHandlerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
